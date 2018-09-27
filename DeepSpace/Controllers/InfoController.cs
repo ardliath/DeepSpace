5110427a3efa8a8d6a037369df16dcdb391825ab
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-09-27 12:56:02 +0100

    Created an info controller

---
+++ DeepSpace/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeepSpace.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "The DeepSpace server is online and operational."; // basic ping message
+        }
+    }
+}
