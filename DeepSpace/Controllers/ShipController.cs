b88d17bdef991984ee764821ea66540cd8e12d1c
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-09-28 13:55:18 +0100

    Created call and test app

---
+++ DeepSpace/Controllers/ShipController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeepSpace.Controllers
+{
+    [Route("api/[controller]")]
+    public class ShipController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "PR new value";
+        //}
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Data value)
+        {
+        }
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+
+    public class Data
+    {
+        public string name { get; set; }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PostDataAsync();
+
+            Console.ReadKey();
+        }
+
+        static async void PostDataAsync()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    //var response = await client.PostAsync("http://localhost:51530/api/ship", new StringContent("{name: Adam}"));
+
+                    //var response = await client.PostAsync("http://localhost:51530/api/ship", new StringContent("{name: Adam}", Encoding.UTF8, "application/json"));
+                    //var text = response.EnsureSuccessStatusCode();
+
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(new Data { name = "Adam" }), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync("http://localhost:51530/api/ship", content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string data = await response.Content.ReadAsStringAsync();
+                        //product = JsonConvert.DeserializeObject<Product>(data);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+    }
+
+        public class Data
+        {
+            public string name { get; set; }
+        }    
+}
