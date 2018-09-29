8d5878d8ed496f6fc3494976d8eb4ecf88b7a7fc
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-09-29 14:29:35 +0100

    Corrected colletion information in data access

--- DeepSpace.Data/ShipDataAccess.cs
+++ DeepSpace.Data/ShipDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using DeepSpace.Contracts;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using System;
 using System.Configuration;
@@ -8,12 +9,12 @@ namespace DeepSpace.Data
     public class ShipDataAccess : IShipDataAccess
     {
         public async void InsertShipAsync(Ship ship)
-        {
+        {            
             var url = ConfigurationManager.AppSettings["DatabaseEndpoint"];
             var key = ConfigurationManager.AppSettings["DatabaseKey"];
             using (var client = new DocumentClient(new Uri(url), key))
             {
-                await client.CreateDocumentAsync(UriFactory.CreateDocumentUri("ahgcosmos", "Entities", ship.TransponderCode), ship);
+                Document shipResponse = await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("DeepSpace", "Entities"), ship);
             }
         }
     }
