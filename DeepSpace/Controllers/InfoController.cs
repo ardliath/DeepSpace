e961505bccdbf84ed378c4388d86e0b70d6d04de
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-09-30 15:59:16 +0100

    Rolled back info changes

--- DeepSpace/Controllers/InfoController.cs
+++ DeepSpace/Controllers/InfoController.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 
 namespace DeepSpace.Controllers
 {
     [Route("api/[controller]")]
     public class InfoController : Controller
     {
-        public InfoController(IConfiguration configuration)
-        {
-            this.Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // GET api/values
         [HttpGet]
         public string Get()
         {
-            var endpoint = this.Configuration.GetValue<string>("DatabaseEndpoint");
-            return $"The DeepSpace server is online, operational, and connected to {endpoint}."; // basic ping message
+            return "The DeepSpace server is online and operational."; // basic ping message
         }
     }
 }
