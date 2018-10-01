1d0c42f231337a972c11272963579674da695f90
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-10-01 12:18:46 +0100

    Added new request/responses

---
+++ DeepSpace/Messages/MoveShipRequest.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeepSpace.Messages
+{
+    public class MoveShipRequest
+    {
+        [JsonProperty(PropertyName = "commandCode")]
+        public string CommandCode { get; set; }
+
+        [JsonProperty(PropertyName = "destination")]
+        public LocationRequestOrResponse Destination { get; set; }
+    }
+}
