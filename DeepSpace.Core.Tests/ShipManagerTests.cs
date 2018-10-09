1200967c0c5e3e77a7216a431070608a44c5e031
Max K <mxss1998@yandex.ru>
2018-10-09 22:54:24 +0300

    GetDistance negative numbers tests

--- DeepSpace.Core.Tests/ShipManagerTests.cs
+++ DeepSpace.Core.Tests/ShipManagerTests.cs
@@ -19,6 +19,30 @@ namespace DeepSpace.Core.Tests
                 Z = 0
             };
             Assert.IsTrue(ShipManager.GetDistance(location1, location2) == 2);
+            
+            location1 = new Location() {
+                X = 0,
+                Y = 0,
+                Z = -2
+            };
+            location2 = new Location() {
+                X = 0,
+                Y = 0,
+                Z = -1
+            };
+            Assert.IsTrue(ShipManager.GetDistance(location1, location2) == 1);
+            
+            location1 = new Location() {
+                X = 0,
+                Y = 5,
+                Z = 0
+            };
+            location2 = new Location() {
+                X = 0,
+                Y = -2,
+                Z = 0
+            };
+            Assert.IsTrue(ShipManager.GetDistance(location1, location2) == 7);
         }
     }
 }
