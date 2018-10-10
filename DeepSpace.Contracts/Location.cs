07f241ef703faac0ef2b5aa951b9967b273fd028
Max K <mxss1998@yandex.ru>
2018-10-10 21:09:20 +0300

    Location constructor x,y,z default values

--- DeepSpace.Contracts/Location.cs
+++ DeepSpace.Contracts/Location.cs
@@ -6,7 +6,7 @@
         public decimal Y { get; set; }
         public decimal Z { get; set; }
 
-        public Location(decimal setX, decimal setY, decimal setZ) {
+        public Location(decimal setX = 0, decimal setY = 0, decimal setZ = 0) {
             X = setX;
             Y = setY;
             Z = setZ;
